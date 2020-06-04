0e81a260e03d908fd8ba7c1eee2b37da4fd49e08
KFAaron <love007zhong@163.com>
2020-06-04 21:55:13 +0800

    添加首页内容

--- DemoForSystematism/DemoForSystematism/ViewController.m
+++ DemoForSystematism/DemoForSystematism/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString * cellIdentifier = @"cellIdentifier";
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (nonatomic, copy) NSDictionary *sourceDic;
+@property (nonatomic, copy) NSArray *titleArr;
 
 @end
 
@@ -16,10 +23,61 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    
     
 }
 
+#pragma mark - UITableViewDelegate And UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return self.titleArr.count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return [self.titleArr[section] substringFromIndex:3];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSString *title = self.titleArr[section];
+    NSArray *rows = self.sourceDic[title];
+    return rows.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    NSString *title = self.titleArr[indexPath.section];
+    NSArray *rows = self.sourceDic[title];
+    cell.textLabel.text = rows[indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+#pragma mark - Propreties
+
+- (NSDictionary *)sourceDic {
+    if (!_sourceDic) {
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"mainResource" ofType:@"json"];
+        NSData *data = [NSData dataWithContentsOfFile:path];
+        NSError *error;
+        NSDictionary *sourceDic = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+        _sourceDic = sourceDic;
+    }
+    return _sourceDic;
+}
+
+- (NSArray *)titleArr {
+    if (!_titleArr) {
+        NSArray *titleArr = [self.sourceDic.allKeys sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+            return [(NSString *)obj1 compare:(NSString *)obj2];
+        }];
+        _titleArr = titleArr;
+    }
+    return _titleArr;
+}
 
 @end
